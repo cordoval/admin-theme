1747e090f608d2bf19dda85a73fc2dcfbed52ec2
Martin Rusev <martinrusev@zoho.com>
2010-08-24 14:57:39 +0300

    First version

---
+++ config.rb
@@ -0,0 +1,13 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+environment = :development
+line_comments = true
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "src"
+images_dir = "images"
+output_style = :compact
+sass_options = {:debug_info => true}
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
